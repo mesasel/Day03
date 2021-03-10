b574780b413981d9785da544b2f0bc364dad612f
Meša Selimović <mesa_sel@hotmail.com>
2021-03-10 22:49:16 +0100

    Suma dva broja

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var sum = numOne + numTwo;
+    // Only change code above this line
+    return sum;
+    
+    }
+    
+    console.log(main(15, 8),main(5, 28),main(150,425)); // change this line
+
+    module.exports = main;
