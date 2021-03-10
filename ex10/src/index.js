83e813e405272749b13e9e755127046671d16dd5
Meša Selimović <mesa_sel@hotmail.com>
2021-03-10 23:17:01 +0100

    Mnozenje varijabli

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    // Only change code above this line
+    return product;
+    
+    }
+    
+    console.log(main(15, 3),main(25, 8),main(425,10)); // change this line
+
+    module.exports = main;
