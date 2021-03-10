16dacbce1edf1a08cea4826ec42aed5e78706525
Meša Selimović <mesa_sel@hotmail.com>
2021-03-10 23:08:55 +0100

    Oduzimanje dva broja

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var difference = numOne - numTwo;
+    // Only change code above this line
+    return difference;
+    
+    }
+    
+    console.log(main(15, 8),main(25, 8),main(425,120)); // change this line
+
+    module.exports = main;
