c2b7a81308505e82a9e0e24f6f8ae5735b755e86
Meša Selimović <mesa_sel@hotmail.com>
2021-03-10 20:23:30 +0100

    Dodate dvije vrste komentara

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+// Ovo je komentar broj jedan
+/* Ovo je komentar broj dva */
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
